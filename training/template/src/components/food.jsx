7d7faade30f2ac637e89f9fc5ba893ffd0879887
tommycravens <joe.t.cravens92@gmail.com>
2022-04-01 09:42:34 -0400

    added food component

---
+++ training/template/src/components/food.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Food extends Component {
+  render () {
+    return (
+      <div>
+        My favorite food is Texas Brisket.
+      </div>
+    )
+  }
+}
+
+export default Food
